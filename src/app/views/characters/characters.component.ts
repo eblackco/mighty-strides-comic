fbc78c3d6ec444a8cee9006b2ef6d71e7c9e4837
bitacoustic <bitacoustic@gmail.com>
2019-11-05 22:55:06 -0800

    added characters multi-tag search and pages display

--- src/app/views/characters/characters.component.ts
+++ src/app/views/characters/characters.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from 'src/app/services/content.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-characters',
@@ -8,11 +9,42 @@ import { ContentService } from 'src/app/services/content.service';
 })
 export class CharactersComponent implements OnInit {
   characters;
+  selectedCharacters = [];
 
-  constructor(private contentService: ContentService) { }
+  constructor(
+    private contentService: ContentService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.characters = this.contentService.getCharacters();
+  }
+
+  selectCharacter(event, index: number) {
+    let tag = this.characters[index]['tag'];
+    let pos = this.selectedCharacters.indexOf(tag);
+
+    if (pos < 0) {
+      this.selectedCharacters.push(tag);
+      event.target.classList.add('active');
+    } else {
+      this.selectedCharacters.splice(pos);
+      event.target.classList.remove('active');
+    }
+
+    // console.log('selected characters', this.selectedCharacters);
+  }
+
+  searchSelectedCharacters() {
+    this.router.navigate(['/pages'], { queryParams: { tags: this.selectedCharacters.toString() }});
+  }
+
+  resetSelectedCharacters() {
+    let characters = document.getElementsByClassName('character');
+    for (let i = 0; i < characters.length; i++) {
+      characters[i].classList.remove('active');
+    }
+    this.selectedCharacters = [];
   }
 
 }
--- src/app/views/pages/pages.component.ts
+++ src/app/views/pages/pages.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from 'src/app/services/content.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -9,10 +10,46 @@ import { ContentService } from 'src/app/services/content.service';
 export class PagesComponent implements OnInit {
   pages;
 
-  constructor(private contentService: ContentService) { }
+  hasTags: boolean = false;
+  tags;
+
+  constructor(
+    private contentService: ContentService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.pages = this.contentService.getPages();
+    this.route.queryParams.subscribe(params => {
+      if (params['tags']) {
+        this.hasTags = true;
+        this.tags = params['tags'].split(',');
+      } else {
+        this.hasTags = false;
+      }
+    })
   }
 
+  getTags() {
+    return this.tags.toString().replace(',', ', ');
+  }
+
+  tagsMatch(pageIndex: number): boolean {
+    let match = true;
+
+    for (let i = 0; i < this.tags.length; i++) {
+      console.log('evaluate', this.tags[i], 'in page', this.pages[pageIndex]);
+      if (this.pages[pageIndex].tags.indexOf(this.tags[i]) < 0) {
+        match = false;
+      }
+    }
+
+    console.log('match', match)
+    return match;
+  }
+
+  seeAllPages() {
+    this.router.navigate(['/pages']);
+  }
 }
